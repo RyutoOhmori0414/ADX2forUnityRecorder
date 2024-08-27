88e8adfa9c72ea201085b9c9aa2e76f7c2963572
VGA-TKY-DT24049\vantan <ryutoomori0414@gmail.com>
2024-08-27 15:33:21 +0900

    [add]Package

---
+++ Assets/Editor/OMCHeadingDrawer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace OMC.ShaderLibrary.Editor
+{
+    public class OMCHeadingDrawer : MaterialPropertyDrawer
+    {
+        private readonly string _headingLabel;
+
+        public OMCHeadingDrawer(string headingLabel)
+        {
+            _headingLabel = headingLabel;
+        }
+
+        public override void Apply(MaterialProperty prop)
+        {
+        }
+
+        public override void OnGUI(Rect position, MaterialProperty prop, GUIContent label, MaterialEditor editor)
+        {
+        }
+    }
+}
